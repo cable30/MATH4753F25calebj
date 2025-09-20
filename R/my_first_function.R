1b18e6de7bedb7bbaee4cd9e77350175b6c7dd7a
Caleb <cjud30@gmail.com>
2025-09-19 21:01:26 -0500

    added description

--- R/my_first_function.R
+++ R/my_first_function.R
@@ -1,6 +1,6 @@
 #' My first function
 #'
-#' @param inp
+#' @param inp the input, a vector
 #'
 #' @returns A list of the values where x are the inputs and y is the square of the inputs
 #' @export
@@ -12,3 +12,4 @@ my_first_function <- function(inp){
   plot(y ~ inp)
   list(x = inp, y = y)#the last line of the body of the function is what is going to be returned
 }
+#add a function called "mysum" that will take a vector and create a summary of the vector, document it
